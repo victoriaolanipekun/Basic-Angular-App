eb9c0137f6517e02ee5f1de866ca4bb845afdebe
Victoria Olanipekun <queentoria1@gmail.com>
2021-11-03 19:00:13 +0000

    created home component filter

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public sort: string;
+  constructor() {
+    this.sort = ''
+   }
 
   ngOnInit(): void {
   }
