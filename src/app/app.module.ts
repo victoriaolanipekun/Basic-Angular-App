b6cdbefe4f4a5611432b16e67e9abe8cde5e812f
Victoria Olanipekun <queentoria1@gmail.com>
2022-03-02 00:35:06 +0000

    home component, service and interceptor created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { GaugeModule } from 'angular-gauge';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
@@ -14,6 +14,8 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
+import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
 
 @NgModule({
   declarations: [
@@ -33,7 +35,18 @@ import { HomeComponent } from './components/home/home.component';
     MatFormFieldModule,
     MatSelectModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/interceptors/http-errors.interceptor.ts
+++ src/app/interceptors/http-errors.interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, throwError as observableThrowError } from 'rxjs';
+import {catchError} from 'rxjs/operators';
 
 @Injectable()
 export class HttpErrorsInterceptor implements HttpInterceptor {
@@ -11,10 +12,10 @@ export class HttpErrorsInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
-      catchError(err) => {
+      catchError((err) => {
         console.log(err);
         return observableThrowError(err);
-      }
+      })
     )
   }
 }
